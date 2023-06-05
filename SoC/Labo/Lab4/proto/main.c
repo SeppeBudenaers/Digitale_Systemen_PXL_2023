384d14c1fbe82ab5cf162cf4761ea3e7ff438a08
SeppeBudenaers <12103946@student.pxl.be>
2023-06-05 18:06:19 +0200

    lab4_5
    it works just set on 100hz because its easier to visualze

--- SoC/Labo/Lab4/proto/main.c
+++ SoC/Labo/Lab4/proto/main.c
@@ -1,67 +1,65 @@
+/*******************************************************************************
+* Header Files
+*******************************************************************************/
 #include "cyhal.h"
 #include "cybsp.h"
+cyhal_timer_t timer_obj;
+cyhal_dac_t my_dac_obj;
 
-#define NUM_CHANNELS 2
+uint16_t dac_value = 0;
+uint16_t dac_read;
 
-static cyhal_adc_channel_t adc_channels[NUM_CHANNELS];
-
-static void adc_continuous_event_handler(void* arg, cyhal_adc_event_t event)
+static void isr_timer(void *callback_arg, cyhal_timer_event_t event)
 {
-    CY_UNUSED_PARAMETER(arg);
-    CY_UNUSED_PARAMETER(event);
-    /* Note: arg is configured below to be a pointer to the adc object */
-    if(0u != (event & CYHAL_ADC_EOS))
-    {
-        for(int i = 0; i < NUM_CHANNELS; ++i)
-        {
-            /* Perform application-specific processing of the ADC result */
-            process_adc_result(i, cyhal_adc_read(&adc_channels[i]));
-            cyhal_gpio_toggle(P1_1);
-            cyhal_system_delay_ms(100);
-
-        }
-    }
+	dac_read = cyhal_dac_read(&my_dac_obj);
+	dac_value = dac_read + 66;
 }
 
-void snippet_cyhal_adc_continuous_read(void)
+void snippet_cyhal_timer_event_interrupt()
 {
-    cyhal_adc_t adc_obj;
-    /* Initialize the each member of the adc_channels array as shown in Snippet 2 */
-    cyhal_adc_config_t config = { /* Other configuration options are omitted here for clarity */ .continuous_scanning = true };
-    /* Register a callback and set the callback argument to be a pointer to the adc object
-     * so that we can easily reference it in the event handler. */
-     cyhal_adc_register_callback(&adc_obj, &adc_continuous_event_handler, &adc_obj);
-    /* Subscribe to the end of scan event so that we we can process the results as each scan completes*/
-    cyhal_adc_enable_event(&adc_obj, CYHAL_ADC_EOS, CYHAL_ISR_PRIORITY_DEFAULT, true);
-    /* Call the ADC configure function to begin continuous scanning */
-    cyhal_adc_configure(&adc_obj, &config);
+    cy_rslt_t rslt;
+    const cyhal_timer_cfg_t timer_cfg =
+    {
+        .compare_value = 0,                 /* Timer compare value, not used */
+        .period = 1000,                      /* Defines the timer period */
+        .direction = CYHAL_TIMER_DIR_UP,    /* Timer counts up */
+        .is_compare = false,                /* Don't use compare mode */
+        .is_continuous = true,              /* Run the timer indefinitely */
+        .value = 0                          /* Initial value of counter */
+    };
+    /* Initialize the timer object. Does not use pin output ('pin' is NC) and
+     * does not use a pre-configured clock source ('clk' is NULL). */
+    rslt = cyhal_timer_init(&timer_obj, NC, NULL);
+    CY_ASSERT(CY_RSLT_SUCCESS == rslt);
+    /* Apply timer configuration such as period, count direction, run mode, etc. */
+    rslt = cyhal_timer_configure(&timer_obj, &timer_cfg);
+    /* Set the frequency of timer to 10000 Hz */
+    rslt = cyhal_timer_set_frequency(&timer_obj, (1000*10000));
+    /* Assign the ISR to execute on timer interrupt */
+    cyhal_timer_register_callback(&timer_obj, isr_timer, NULL);
+    /* Set the event on which timer interrupt occurs and enable it */
+    cyhal_timer_enable_event(&timer_obj, CYHAL_TIMER_IRQ_TERMINAL_COUNT, 3, true);
+    /* Start the timer with the configured settings */
+    rslt = cyhal_timer_start(&timer_obj);
 }
 
 int main(void)
 {
-	cybsp_init();
-	__enable_irq();
-    cy_rslt_t rslt;
-    cyhal_gpio_init(P1_1, CYHAL_GPIO_DIR_OUTPUT,CYHAL_GPIO_DRIVE_STRONG , 0);
-    cyhal_adc_t adc_obj;
-    cyhal_adc_channel_t adc_chan_0_obj;
-    cyhal_adc_channel_t adc_chan_1_obj;
-    /* 32-bit ADC conversion result */
-    int32_t adc_out_0;
-    int32_t adc_out_1;
-    /* Use the same configuration for both channels */
-    const cyhal_adc_channel_config_t channel_config = { .enable_averaging = false, .min_acquisition_ns = 220, .enabled = true };
-    /* Initialize ADC. The ADC block which can connect to pin 10[0] is selected */
-    rslt = cyhal_adc_init(&adc_obj, P10_0, NULL);
-    /* Initialize a channel to scan P10_0 with the default vminus */
-    rslt = cyhal_adc_channel_init_diff(&adc_chan_0_obj, &adc_obj, P10_0, CYHAL_ADC_VNEG, &channel_config);
-    /* Initialize a differential channel for the pair of P10_2 and P10_3 */
-    rslt = cyhal_adc_channel_init_diff(&adc_chan_1_obj, &adc_obj, P10_2, P10_3, &channel_config);
-    /* Read the ADC conversion results for both channels. Repeat as necessary. */
-    adc_out_0 = cyhal_adc_read(&adc_chan_0_obj);
-    adc_out_1 = cyhal_adc_read(&adc_chan_1_obj);
+    /* Initialize the device and board peripherals */
+    cybsp_init();
+    /* Enable global interrupts */
+    __enable_irq();
+
+
+    /* Full Scale value for the DAC */
+
+    /* Initialize DAC, set Pin 9[6] as the DAC output */
+    cyhal_dac_init(&my_dac_obj, P9_6);
 
     for (;;)
     {
+        /* Write the 16 bit value as DAC input */
+        cyhal_dac_write(&my_dac_obj, dac_value);
     }
 }
+/* [] END OF FILE */
